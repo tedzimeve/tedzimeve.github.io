ad8493d1a46c274305e68727285696da8bf56dd3
tedzimeve <anonwithout@yandex.ru>
2022-10-15 19:46:38 +0500

    vk api test

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var group_id = '199380286';
 
 var API_URL = 'https://api.vk.com/method/';
 
-method = {
+api = {
     'wall': {
-        'get': "wall.get"
+        'get': API_URL+"wall.get"
     }
 }
 
+var and = "&access_token=vk1.a.JM5JmVhanz37WXic9_UwbtRNCGRUjDO4MOBjKKkaRXfAnS5mNauHEEmeBcr_hZhkr6YTu3Eks-SLdLE3UezMJTGZkwyU2ejU9VeuovaTe5dV4ZZBkKXGY6gp8zVDCa5MSiEYiNHYi7IvHDHIvDIAIa86fVFO4nFsDbf03rGkvTdAWfuq1RiAct_29JK9-hfJ&v=5.131";
+function apim(url, properties) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url+'?'+properties+and);
+    return req.send(null);
+}
 
-//wall.get?owner_id=199380286&access_token=vk1.a.JM5JmVhanz37WXic9_UwbtRNCGRUjDO4MOBjKKkaRXfAnS5mNauHEEmeBcr_hZhkr6YTu3Eks-SLdLE3UezMJTGZkwyU2ejU9VeuovaTe5dV4ZZBkKXGY6gp8zVDCa5MSiEYiNHYi7IvHDHIvDIAIa86fVFO4nFsDbf03rGkvTdAWfuq1RiAct_29JK9-hfJ&v=5.131
+//wall.get?owner_id=199380286
