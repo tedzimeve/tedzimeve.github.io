0c2014f0afde9810757100dcab671affbd05980a
tedzimeve <anonwithout@yandex.ru>
2022-10-15 18:54:53 +0500

    modified:   index.html 	new file:   index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+var group_id = '199380286';
+
+var API_URL = 'https://api.vk.com/method/';
+
+method = {
+    'wall': {
+        'get': "wall.get"
+    }
+}
+
+
+//wall.get?owner_id=199380286&access_token=vk1.a.JM5JmVhanz37WXic9_UwbtRNCGRUjDO4MOBjKKkaRXfAnS5mNauHEEmeBcr_hZhkr6YTu3Eks-SLdLE3UezMJTGZkwyU2ejU9VeuovaTe5dV4ZZBkKXGY6gp8zVDCa5MSiEYiNHYi7IvHDHIvDIAIa86fVFO4nFsDbf03rGkvTdAWfuq1RiAct_29JK9-hfJ&v=5.131
